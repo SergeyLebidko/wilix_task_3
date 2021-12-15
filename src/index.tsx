79a14d9504a21918f421341af9138dac6377210a
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-15 17:57:57 +0300

    Добавлен код сброса стилей

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import {render} from 'react-dom';
 import App from './components/App/App';
+import './styles/init.scss';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
