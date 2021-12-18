44b4358871770b1fca0dfce9e688006a2a53ee0a
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-18 11:08:11 +0300

    Добавлена обработка ошибок парсера

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -11,17 +11,42 @@ const Converter: React.FC<ConverterProps> = ({ratesData}) => {
     const rates: RateRecord = ratesData.rates;
     const rateCodes: Array<string> = Object.keys(rates);
 
+    const [error, setError] = useState<string | null>(null);
     const [result, setResult] = useState<number | null>();
     const inputRef = useRef<HTMLInputElement>(null);
+
+    const setErrorText = (text: string): void => {
+        setError(text);
+        setTimeout(() => setError(null), 4000);
+    }
 
     const calcHandler = (): void => {
         if (!inputRef.current) return;
 
         const {base, rates} = ratesData;
         const [_count, _baseFrom, _baseTo] = inputRef.current.value.split(/\s+/);
+        if (!_count || !_baseFrom || !_baseTo) {
+            setErrorText('Введите сумму, код исходной валюты и код целевой валюты');
+            return;
+        }
+
         const count = +_count;
+        if (isNaN(count)) {
+            setErrorText('Введите корректную сумму');
+            return;
+        }
+
         const baseFrom = _baseFrom.toUpperCase();
+        if (!rateCodes.includes(baseFrom)) {
+            setErrorText('Код исходной валюты некорректен');
+            return;
+        }
+
         const baseTo = _baseTo.toUpperCase();
+        if (!rateCodes.includes(baseTo)) {
+            setErrorText('Код целевой валюты некорректен');
+            return;
+        }
 
         if (baseFrom === baseTo) {
             setResult(count);
@@ -29,7 +54,8 @@ const Converter: React.FC<ConverterProps> = ({ratesData}) => {
         }
 
         if (baseTo === base) {
-            setResult(count / rates[base]);
+            console.log('тут', count)
+            setResult(count / rates[baseFrom]);
             return;
         }
 
@@ -42,7 +68,7 @@ const Converter: React.FC<ConverterProps> = ({ratesData}) => {
     }
 
     const cardClickHandler = (code: string): void => {
-        if(!inputRef.current) return;
+        if (!inputRef.current) return;
         inputRef.current.value += ` ${code}`;
     }
 
@@ -52,6 +78,7 @@ const Converter: React.FC<ConverterProps> = ({ratesData}) => {
                 <input className="converter__request_field" ref={inputRef} placeholder="Введите запрос"/>
                 <button className="converter__calc_button" onClick={calcHandler}>Рассчитать</button>
             </div>
+            {error && <p className="converter__error_text">{error}</p>}
             <p className="converter__help_text">
                 Введите запрос на конвертирование в виде: сумма код_исходной_валюты код_целевой_валюты
             </p>
